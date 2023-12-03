0c969b092c3bc8bc226571a225710ae66fe7d480
Developer Shohan <dev.shohanur@gmail.com>
2023-12-03 20:41:11 +0600

    nested routing practice done

--- src/Router/privateRouter.jsx
+++ src/Router/privateRouter.jsx
@@ -1,20 +1,36 @@
-import Layout from "../components/Layout/Layout"
-import Admin from "../pages/Admin/Admin"
-import Blog from "../pages/Admin/pages/blog/Blog"
+import Layout from "../components/Layout/Layout";
+import Admin from "../pages/Admin/Admin";
+import Post from "../pages/Admin/pages/Post/Post";
+import Blog from "../pages/Admin/pages/blog/Blog";
+import Category from "../pages/Admin/pages/category/Category";
 
 
-const privateRouter = [{
-    element: <Layout/>,
-    children: [{
-        path: "/admin",
-        element: <Admin/>,
-        children:[
-{
-    path: "/blog",
-    element: <Blog/>,
-}
-        ]
-    }]
-}]
 
-export default privateRouter
+
+const privateRouter = [
+	{
+		element: <Layout />,
+		children: [
+			{
+				path: "/admin",
+				element: <Admin />,
+				children: [
+					{
+						path: "blog",
+						element: <Blog />,
+					},
+					{
+						path: "post",
+						element: <Post />,
+					},
+					{
+						path: "category",
+						element: <Category />,
+					},
+				],
+			},
+		],
+	},
+];
+
+export default privateRouter;
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,27 +1,47 @@
-import { Link, Outlet } from "react-router-dom"
-import "./Admin.scss"
+import { Link, Outlet, useLocation } from "react-router-dom";
+import "./Admin.scss";
+import { checkPath } from "../../helper/Helper";
 
 const Admin = () => {
-  return (
-    <div>Admin
-    
-<div className="admin_container">
-  <div className="admin_left_con">
-    <ul>
-      <li className=" admin_sub_link">
-        <Link to="/blog">Blog</Link>
-        <Link to="/post">post</Link>
-        <Link to="/category">category</Link>
-      </li>
-    </ul>
-  </div>
-  <div className="admin_right_con">
-    <Outlet/>
-  </div>
-</div>
-    
-    </div>
-  )
-}
+	const location = useLocation();
 
-export default Admin
+	return (
+		<div>
+			Admin
+			<div className="admin_container">
+				<div className="admin_left_con">
+					<ul>
+						<li className=" admin_sub_link">
+							<Link
+								to="blog"
+								className={
+									checkPath(location.pathname, "blog") ? "active" : ""
+								}>
+								Blog
+							</Link>
+							<Link
+								to="post"
+								className={
+									checkPath(location.pathname, "post") ? "active" : ""
+								}>
+								post
+							</Link>
+							<Link
+								to="category"
+								className={
+									checkPath(location.pathname, "category") ? "active" : ""
+								}>
+								category
+							</Link>
+						</li>
+					</ul>
+				</div>
+				<div className="admin_right_con">
+					<Outlet />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Admin;
