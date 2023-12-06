cccbd04bfbed38bf1795d7cd3ddb342681c60400
Developer Shohan <dev.shohanur@gmail.com>
2023-12-06 23:29:43 +0600

    practice useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.scss'
 import { RouterProvider } from 'react-router-dom'
 import router from './Router/Router'
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,48 @@
-
-
+import { useState } from "react";
+import "./About.scss";
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
 
-export default About
+const [count, setCount] = useState(0)
+const [msg, setMsg] = useState("hi")
+
+
+
+
+	return (
+		<div>
+			About
+			<div className="card text-left">
+				<img className="card-img-top" src="holder.js/100px180/" alt="" />
+				<div className="card-body">
+					<h4 className="card-title">Title</h4>
+					<h4 className="counter-value">{count}</h4>
+					<div className="counter-btns d-flex gap-3 mt-3">
+						<button type="button" className="btn btn-success" onClick={()=>
+              setCount((prevState)=> prevState + 1)
+            }>
+							++
+						</button>
+						<button type="button" className="btn btn-info text-light " onClick={()=> setCount(0)}>
+							Reset
+						</button>
+						<button type="button" className="btn btn-danger" onClick={()=> count > 0 ? setCount((prevState)=> prevState - 1): setCount(0) }>
+							--
+						</button>
+					</div>
+				</div>
+			</div>
+
+			<div className="card">
+				<img className="card-img-top" src="holder.js/100x180/" alt=""/>
+				<div className="card-body">
+					<h4 className="card-title">{msg}</h4>
+
+					<input type="text" onChange={(e)=> setMsg(e.target.value)}/>
+
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default About;
